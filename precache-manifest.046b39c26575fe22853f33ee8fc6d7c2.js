e549e0f3a30f747e366c9381eae47c9ddca850f6
Dmitriy Nikishin <51823084+nik-di@users.noreply.github.com>
2020-10-26 22:19:05 +0300

    Updates

---
+++ precache-manifest.046b39c26575fe22853f33ee8fc6d7c2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a245a1ce2020a4a48e61f278569946",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d15fe9e73cf6ac72365",
+    "url": "./static/css/main.62912976.chunk.css"
+  },
+  {
+    "revision": "b98bbcfce2323db8098c",
+    "url": "./static/js/2.8beaf655.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.8beaf655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d15fe9e73cf6ac72365",
+    "url": "./static/js/main.50879695.chunk.js"
+  },
+  {
+    "revision": "a8ef13021948c1ee7e9c",
+    "url": "./static/js/runtime-main.a5163ae5.js"
+  },
+  {
+    "revision": "a3e3d9bd5607b846d430dbc9afa3845e",
+    "url": "./static/media/icon-close.a3e3d9bd.svg"
+  },
+  {
+    "revision": "429c87524d1c2a67d40e245d3aef7551",
+    "url": "./static/media/like-active.429c8752.svg"
+  },
+  {
+    "revision": "ed9e50d5224dd8511042e16f2a4dba2f",
+    "url": "./static/media/like-inactive.ed9e50d5.svg"
+  },
+  {
+    "revision": "9d0e3a9396c882c10d40bb68c25ec9ad",
+    "url": "./static/media/logo.9d0e3a93.svg"
+  },
+  {
+    "revision": "18e456c4258815094dbce13755c27006",
+    "url": "./static/media/trash-icon.18e456c4.svg"
+  }
+]);
